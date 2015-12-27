63acceaae39aa09cbba02d742ddf6fcac3ace265
github-jef <jimbo.fox@hotmail.co.uk>
2015-12-27 17:10:31 +0000

    functions created as per assignment instructions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R functions for caching potential time consuming computations
+## In this example finding the inverse of a matrix
 
-## Write a short comment describing this function
+## Creates a "vector" that contains a function to store the inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Calculate the inverse, but first check to see whether has
+## already been calculated and if so retrieve from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
